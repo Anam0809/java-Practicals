a773cc42e30e9d776e618dfced9cfbab30155d91
Anam0809 <anamshaikh4163@gmail.com>
2026-01-31 21:41:10 +0530

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        int a=2;
+        if (a%2==0){
+        System.out.println("Even Number");
+        }
+        else{
+       System.out.println("Odd Number");
+
+        }
+    }
+}
